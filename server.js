193bd0d7f822dff96be9c11d0da2e9f38dc25db2
Matty Watts <watts.mathew@gmail.com>
2020-01-25 13:11:22 -0600

    setting up server file

---
+++ server.js
@@ -0,0 +1,19 @@
+const db = require('./models');
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./public"));
+
+require('./routes/api-routes')(app);
+
+db.sequelize.sync().then(function () {
+
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+
+});
