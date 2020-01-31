2308df70e75fb99b039f212de5826efbb7d8fca5
Matty Watts <watts.mathew@gmail.com>
2020-01-30 18:23:58 -0600

    adding api routes and html

---
+++ views/employee.js
@@ -0,0 +1,10 @@
+const render = function(data){
+    return `
+    <input type="text" placeholder="First Name">
+<input type="text" placeholder="Last Name">
+<input type="text" placeholder="Employee's Email">
+<button type="button">submit</button>
+    `
+}
+
+exports.render = render;
--- views/layouts/homePage.js
+++ views/layouts/homePage.js
@@ -6,7 +6,7 @@ const render = function (data) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="css/style.css">
+        <link type="text/css" rel="stylesheet" href="/css/style.css">
         <!--google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Kanit:300&display=swap" rel="stylesheet">
         <title>Time Clock</title>
@@ -25,7 +25,7 @@ const render = function (data) {
         src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
-    <script src="js/buttons.js"></script>
+    <script src="/js/buttons.js"></script>
     </body>
     </html>`
 };
