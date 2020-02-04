9530cbcc212b62bdd26f721d71cace287cccc1a1
Corbin Herrera <corbin.h.mt@gmail.com>
2020-02-03 22:36:12 -0600

    Button Changes

--- views/settings.js
+++ views/settings.js
@@ -1,13 +1,14 @@
 const render = function(){
     return `
-    <section class="hero is-fullheight">
-        <div class="columns">
-            <button type='button' id='addDepartment'>add department</button>
-            <button type="button" id='employeePage'>add employee</button>
+    <section id="containerForSettings" class="container">
+        <div id="columnsForSettings" class="columns">
+            <button type='button' class="color-change" id='addDepartment'>add department</button>
+            <button type="button" class="color-change" id='employeePage'>add employee</button>
+            
         </div>
         <div class="columns">
-            <button type="button" id='deleteEmployee'>delete employee</button>
-            <button type="button" id='viewEmployees'>view all employees</button>
+            <button type="button" class="color-change" id='deleteEmployee'>delete employee</button>
+            <button type="button" class="color-change" id='viewEmployees'>view all employees</button>
         </div>
     </section>
     `
