54daf98c57cc0f87436b611ad16531fdb3ea0dbb
Corbin Herrera <corbin.h.mt@gmail.com>
2020-02-03 22:37:58 -0600

    css changes?

--- views/settings.js
+++ views/settings.js
@@ -7,12 +7,8 @@ const render = function(){
             
         </div>
         <div class="columns">
-<<<<<<< HEAD
             <button type="button" class="color-change" id='deleteEmployee'>delete employee</button>
             <button type="button" class="color-change" id='viewEmployees'>view all employees</button>
-=======
-            <button type="button" id='viewEmployees'>view all employees</button>
->>>>>>> 8a1b00414715f5d7b950f1e0ca588791de0a65bf
         </div>
     </section>
     `
