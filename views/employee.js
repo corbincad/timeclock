259883d2e58f0785eb0d60c67a9228f9759fed51
Corbin Herrera <corbin.h.mt@gmail.com>
2020-02-04 19:45:03 -0600

    More CSS changes

--- views/employee.js
+++ views/employee.js
@@ -1,17 +1,32 @@
 const render = function(data){
     return `
-    <div id="namer">
-            <div id="id-input">
-            <input type="text" name="namername" placeholder="First Name" id="firstName">
-            </div>
 
-<input type="text" placeholder="Last Name" id="lastName">
-<input type="text" placeholder="Employee's Email" id="email">
-<select id="departmentList">
-    <option selected disabled>Choose Department</option>
-    ${data}
-</select>
-<button type="button" id="addEmployee">submit</button>
+
+    <div id="namer">
+        <div id="id-input">
+            <div class="columns">
+                
+                    <input type="text" name="namername" placeholder="First Name" id="firstName">
+                
+                    <input type="text" placeholder="Last Name" id="lastName">
+                
+                    <input type="text" placeholder="Employee's Email" id="email">
+            </div>
+        </div>
+    </div>
+   
+        <div class="columns">
+            <div class="column">
+                <input type="image" src="/logo/check.png" id="addEmployee" class="color-change">
+            </div>
+            
+            <select id="departmentList">
+                <option selected disabled>Choose Department</option>
+                ${data}
+            </select>
+
+        </div>
+    
     `
 }
 
