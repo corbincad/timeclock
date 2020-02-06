2974be21b5a780cbadb56202fa48b1f68b58b7e3
Matty Watts <watts.mathew@gmail.com>
2020-02-06 17:10:05 -0600

    adding clockin/out buttons

--- views/memberPage.js
+++ views/memberPage.js
@@ -1,7 +1,10 @@
 const render = function(data){
+    console.log(data);
     return `
         <h4>Welcome ${data.firstName} ${data.lastName}!</h4>
         
+        <button type='button' id='clockIn'>Clock In</button>
+    <button type='button' id='clockOut'>Clock Out</button>
     `
 }
 
---
+++ views/timeStamp.js
@@ -0,0 +1,6 @@
+const render = function(data) {
+    return `
+    <button type='button' id='clockIn'>Clock In</button>
+    <button type='button' id='clockOut'>Clock Out</button>
+    `
+}
